c5a403de4e7bb106ebfd266f8a82cc782a944c17
LEVI9\vl.novakovic <vl.novakovic@levi9.com>
2024-01-24 13:31:56 +0100

    fix reporters

---
+++ monocart-report-merge.ts
@@ -0,0 +1,25 @@
+import { merge } from 'monocart-reporter';
+
+const reportDataList = [
+  process.cwd() + '/monocart-report-1_4/index.json',
+  process.cwd() + '/monocart-report-2_4/index.json',
+  process.cwd() + '/monocart-report-3_4/index.json',
+  process.cwd() + '/monocart-report-4_4/index.json'
+
+];
+
+merge(reportDataList, {
+  name: 'Playwright E2E Tests',
+  outputFile: 'final-monocart-report/index.html',
+  trend: 'previous-trend.json',
+  attachmentPath: (currentPath) => {
+    const searchStr = '../test-results/';
+    const replaceStr = './data/';
+
+    if (currentPath.startsWith(searchStr)) {
+      return replaceStr + currentPath.slice(searchStr.length);
+    }
+
+    return currentPath;
+  }
+});
