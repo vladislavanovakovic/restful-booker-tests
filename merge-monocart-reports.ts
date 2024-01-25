2385fe87d2007a3645ddc8159365e6757c2c29fc
LEVI9\vl.novakovic <vl.novakovic@levi9.com>
2024-01-25 12:39:45 +0100

    change

--- merge-monocart-reports.ts
+++ merge-monocart-reports.ts
@@ -10,7 +10,7 @@ const reportDataList = [
 
 merge(reportDataList, {
   name: 'Restful booker tests',
-  outputFile: 'final-monocart-report/index.html',
+  outputFile: 'merged-monocart-report/index.html',
   trend: 'previous-trend.json',
   attachmentPath: (currentPath) => {
     const searchStr = '../test-results/';
